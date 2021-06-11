86f2674d7076010b2172c7e7ca68dbdbe6d4518e
Mattias Karlsson <matkar@wcom.se>
2021-06-11 15:05:36 +0200

    Update Spectre.Console to 0.40.0

--- src/Devlead.Console.Template/templates/devleadconsole/Devlead.Console/Commands/Validation/ValidateStringAttribute.cs
+++ src/Devlead.Console.Template/templates/devleadconsole/Devlead.Console/Commands/Validation/ValidateStringAttribute.cs
@@ -6,6 +6,7 @@ namespace Devlead.Console.Commands.Validation
     public class ValidateStringAttribute : ParameterValidationAttribute
     {
         public const int MinimumLength = 3;
+        private static readonly (bool IsString, bool IsMinimumLength, string Value) InvalidStringValue = (false, false, string.Empty);
 
 #nullable disable
         public ValidateStringAttribute() : base(errorMessage: null)
@@ -13,15 +14,26 @@ namespace Devlead.Console.Commands.Validation
         }
 #nullable enable
 
-        public override ValidationResult Validate(ICommandParameterInfo parameterInfo, object? value)
-            => (value as string) switch {
-                { Length: >= MinimumLength }
-                    => ValidationResult.Success(),
+        public override ValidationResult Validate(CommandParameterContext context)
+            => (
+                    context.Value is string stringValue
+                        ? (
+                            IsString: true,
+                            IsMinimumLength: stringValue.Length >= MinimumLength,
+                            Value: stringValue
+                        )
+                        : InvalidStringValue
+                ) switch
+                {
+                    {IsString: true, IsMinimumLength: true}
+                        => ValidationResult.Success(),
 
-                { Length: < MinimumLength }
-                    => ValidationResult.Error($"{parameterInfo?.PropertyName} ({value}) needs to be at least {MinimumLength} characters long."),
+                    {IsString: true, IsMinimumLength: false} invalidValue
+                        => ValidationResult.Error(
+                            $"{context.Parameter.PropertyName} ({invalidValue.Value}) needs to be at least {MinimumLength} characters long was {invalidValue.Value.Length}."),
 
-                _ => ValidationResult.Error($"Invalid {parameterInfo?.PropertyName} ({value}) specified.")
-            };
+                    _ => ValidationResult.Error(
+                        $"Invalid {context.Parameter.PropertyName} ({context.Value ?? "<null>"}) specified.")
+                };
     }
 }
