df9150245e301deec41bca3854e5067b1cc26c31
Mattias Karlsson <matkar@wcom.se>
2025-04-11 13:38:20 +0200

    Refactor Console Command and Settings
    - Removed unused using directives from ConsoleCommand.cs.
    - Corrected namespace from DevleadConsole.Commands.Setting to DevleadConsole.Commands.Settings in ConsoleSettings.cs.
    - Cleaned up formatting and indentation in ConsoleSettings.cs and ValidateStringAttribute.cs for better readability.
    - Updated project file to reflect the corrected namespace for ConsoleSettings.

--- src/Devlead.Console.Template/DevleadConsole/Commands/Settings/ConsoleSettings.cs
+++ src/Devlead.Console.Template/DevleadConsole/Commands/Settings/ConsoleSettings.cs
@@ -1,25 +1,23 @@
 using System.ComponentModel;
-using Spectre.Console.Cli;
 
-namespace DevleadConsole.Commands.Setting
+namespace DevleadConsole.Commands.Settings;
+
+public class ConsoleSettings : CommandSettings
 {
-    public class ConsoleSettings : CommandSettings
-    {
-        [CommandArgument(0, "<mandatory>")]
-        [Description("Mandatory argument")]
-        public string Mandatory { get; set; } = string.Empty;
+    [CommandArgument(0, "<mandatory>")]
+    [Description("Mandatory argument")]
+    public string Mandatory { get; set; } = string.Empty;
 
-        [CommandArgument(1, "[optional]")]
-        [Description("Optional argument")]
-        public string? Optional { get; set; }
+    [CommandArgument(1, "[optional]")]
+    [Description("Optional argument")]
+    public string? Optional { get; set; }
 
-        [CommandOption("--command-option-flag")]
-        [Description("Command option flag.")]
-        public bool CommandOptionFlag { get; set; }
+    [CommandOption("--command-option-flag")]
+    [Description("Command option flag.")]
+    public bool CommandOptionFlag { get; set; }
 
-        [CommandOption("--command-option-value <value>")]
-        [Description("Command option value.")]
-        [ValidateString]
-        public string? CommandOptionValue { get; set; }
-    }
+    [CommandOption("--command-option-value <value>")]
+    [Description("Command option value.")]
+    [ValidateString]
+    public string? CommandOptionValue { get; set; }
 }
